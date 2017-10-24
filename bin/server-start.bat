7bd103c1806522aa9904a1e970a5c905edd1d3d5
MartinCo <MartinCo@yaina.eu>
2017-10-24 16:48:14 +0100

    Cleanup server start script + add keys

--- bin/server-start.bat
+++ bin/server-start.bat
@@ -12,35 +12,37 @@ call server-kill.bat
 REM Build PBO
 call genPBO.bat
 
-REM move up to server dir to make life easier
-pushd ..\testing
+REM move up to root dir to make life easier
+pushd ..
 
 REM Copy yaina.pbo to mpmissions of server and local cache so we don't have to download it
-copy /y ..\..\yaina.Altis.pbo "%SERVER_DIR%\mpmissions"
-copy /y ..\..\yaina.Altis.pbo "%MP_MISSIONS_CACHE_DIR%"
+copy /y ..\yaina.Malden.pbo "%SERVER_DIR%\mpmissions"
+copy /y ..\yaina.Malden.pbo "%MP_MISSIONS_CACHE_DIR%"
 
 echo %SERVER% ^
 	-ip=127.0.0.1 ^
 	-name=server ^
-	"-profiles=%cd%\Temp\server" ^
+	-nosplash ^
+	"-profiles=%cd%\testing\server" ^
 	-port=%PORT% ^
-	"-config=%cd%\server.cfg" ^
+	"-config=%cd%\bin\server.cfg" ^
 	-world=empty ^
 	-autoInit ^
 	-loadMissionToMemory ^
-	-mod=%MODS%
+	-serverMods=%SERVER_MODS%
 
 start "server" ^
 	%SERVER% ^
 	-ip=127.0.0.1 ^
 	-name=server ^
-	"-profiles=%cd%\Temp\server" ^
+    -nosplash ^
+	"-profiles=%cd%\testing\server" ^
 	-port=%PORT% ^
-	"-config=%cd%\server.cfg" ^
+	"-config=%cd%\bin\server.cfg" ^
 	-world=empty ^
 	-autoInit ^
 	-loadMissionToMemory ^
-	-mod=%MODS%
+	-serverMods=%SERVER_MODS%
 timeout /t 10
 
 REM And pop back to start dir
--- bin/settings.bat
+++ bin/settings.bat
@@ -2,6 +2,7 @@
 
 set MP_MISSIONS_CACHE_DIR=C:\Users\Martin\AppData\Local\Arma 3\MPMissionsCache
 set SERVER_DIR=D:\Arma3Server
-set MODS=curator;kart;heli;mark;expansion;dlcbundle;jets;dlcbundle2
+set SERVER_MODS="ServerMods\@Advanced Rappelling;ServerMods\@Advanced Sling Loading;ServerMods\@Advanced Towing;ServerMods\@Advanced Urban Rappelling"
+set MODS="curator;kart;heli;mark;expansion;dlcbundle;jets;dlcbundle2;"
 set SERVER="%SERVER_DIR%\arma3server.exe"
 set PORT=2502
