b500b2b2cad7014ae3d08122d0d7829bf6afa203
MartinCo <MartinCo@yaina.eu>
2018-02-23 14:26:45 +0000

    Split out some more to server

---
+++ client.Altis/Functions/YAINA/General/CfgFunctions_server.hpp
@@ -0,0 +1,25 @@
+class YAINA {
+	tag = "YAINA";
+
+	class General {
+		file = "Functions\YAINA\General";
+		class killedHint { postInit = 1; };
+		class baseCleanupManager { postInit = 1; };
+		class timeManager{ postInit=1; };
+		class hideTerrainObjects{};
+		class showTerrainObjects{};
+		class getPointBetween {};
+		class postInit { postInit = 1; };
+		class markerManager { postInit = 1; };
+		class preInit { preInit = 1; };
+		class dirFromNearestName {};
+		class addRewardPoints {};
+		class globalHint {};
+		class getPosAround {};
+		class loadDB {};
+		class log {};
+		class getAdminLevel {};
+		class addActionMP {};
+		class killLog {};
+    };
+};
