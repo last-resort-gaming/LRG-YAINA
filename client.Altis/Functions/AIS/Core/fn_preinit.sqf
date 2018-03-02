last-resort-gaming/LRG-YAINA

commit a9d57f6b191c6326a07519d430235694a6b8db02

Fix AIS conflicting with ACE

client.Altis/Functions/AIS/Core/fn_preinit.sqf
@@ -12,8 +12,12 @@ if (isServer) then {
 	publicVariable "ais_mobile_medic_stations";
 };
 
-removeAllMissionEventHandlers "Draw3D";
-removeAllMissionEventHandlers "EachFrame";
+if (!ais_ace_shutDown) then {
 
-call AIS_Core_fnc_initEvents;
-AIS_Core_Interaction_Actions = [];
+    removeAllMissionEventHandlers "Draw3D";
+    removeAllMissionEventHandlers "EachFrame";
+
+    call AIS_Core_fnc_initEvents;
+    AIS_Core_Interaction_Actions = [];
+
+};

commit 5c57ddde9b5cadfc221fb180b2d57f1e8d2b9eef

Fix issues introduced by a9d57f6
  - players weren't being killed due to initEvents not starting
  - caused when player joins before server init
  - not waiting for ais_ace_shutDown to be defined
  - move to just seeing if mod there as it's rquired client side

client.Altis/Functions/AIS/Core/fn_preinit.sqf
@@ -1,13 +1,12 @@
 diag_log format ["############## %1 ############## - AIS init started", missionName];
 
-if (isServer) then {
-	ais_ace_shutDown = false;
-	if (isClass (configFile >> "CfgPatches" >> "ace_main")) then {
-		ais_ace_shutDown = true;
-		["AIS: AIS shutdown itself cause ACE mod was detected. ACE and AIS cant work at the same time."] call BIS_fnc_logFormat;
-	};
-	publicVariable "ais_ace_shutDown";
+ais_ace_shutDown = false;
+if (isClass (configFile >> "CfgPatches" >> "ace_main")) then {
+    ais_ace_shutDown = true;
+    ["AIS: AIS shutdown itself cause ACE mod was detected. ACE and AIS cant work at the same time."] call BIS_fnc_logFormat;
+};
 
+if (isServer) then {
 	ais_mobile_medic_stations  = [];
 	publicVariable "ais_mobile_medic_stations";
 };